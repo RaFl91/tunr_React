aeaa5abfd4d652e109ec29ae080e0c886dd99441
RaFl91 <Terrell.Flemings@gmail.com>
2018-12-06 12:12:50 -0500

    working out some kinks

--- tunr_react/settings.py
+++ tunr_react/settings.py
@@ -15,6 +15,14 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# =========================================================================
+REACT_APP_DIR = os.path.join(BASE_DIR, 'client')
+
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build', 'static')
+]
+# =========================================================================
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -75,7 +83,7 @@ WSGI_APPLICATION = 'tunr_react.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tunrReact',
+        'NAME': 'tunrreact',
         'USER': 'tunr_admin',
         'PASSWORD': 'password',
         'HOST': 'localhost'
